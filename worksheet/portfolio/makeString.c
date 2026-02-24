1300bfd9978cdda764f8848833ba14107b6f704f
Mahmoud Mustafa <mahmoudd.ahmedd21@gmail.com>
2026-02-24 04:41:39 +0000

    Portfolio Done

--- worksheet/portfolio/makeString.c
+++ worksheet/portfolio/makeString.c
@@ -1,7 +1,7 @@
 
 /*
-Name:
-Student ID:
+Name: Mahmoud Mustafa
+Student ID: 201868040
 */
 
 #include <stdio.h>
@@ -10,8 +10,15 @@ Student ID:
 int main( int argc, char **argv ) {
 
     // define appropriate data to hold your answer
+    char buffer[64] = "";           // Temporary storage 
 
     // process the command-line data using appropriate string functions
+    for ( int i = 1; i < argc; ++i) {           // Iterates through the provided input
+        strcat(buffer, argv[i]);
+        if (i < argc - 1) {                     // So that the hyphens used are one less than the characters
+            strcat(buffer, "-");                // Creates a string and connect using the hyphen '-' if true
+        }
+    }
 
     printf("%s\n",buffer); // use only this single print statement in your submitted code
 
