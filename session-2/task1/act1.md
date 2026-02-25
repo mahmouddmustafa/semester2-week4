0c185db1bbd3ed7078865ae435946d467efa98c6
Mahmoud Mustafa <mahmoudd.ahmedd21@gmail.com>
2026-02-25 06:06:56 +0000

    Session 2 - Week 4

--- session-2/task1/act1.md
+++ session-2/task1/act1.md
@@ -9,3 +9,5 @@ Question:
 - Hint: Think about how C data is stored in memory. 
 - Test this by changing the second string to something other than "hello".
 
+Answer:
+The %s specifier expects a null-terminated string, so that it prints strings until it hits a \0 byte, since a char array lacks a 'stop' or null character. This results in it reading into adjacent 'garbage' memory, in this case it is the char string.
--- session-2/task2/substring.c
+++ session-2/task2/substring.c
@@ -1,6 +1,9 @@
 /*
  * Find a substring within a given string
- */
+*/
+
+#include <stdio.h>
+#include <string.h>
 
  // create a program to locate a substring "ump" within a given string "The quick brown fox jumped over the lazy dog"
 
@@ -12,4 +15,21 @@
  5. implement the solution and compute the answer  
  6. print the answer appropriately as pointer, character and string
  */ 
+
+ int main(void) {
+    char str1[100] = "The quick brown fox jumped over the lazy dog";
+    char search[] = "ump";
+
+    char *sub = strstr(str1, search);
+
+    if (sub != NULL) {
+        printf("As a pointer (memory address): %p\n", (void*)sub);
+        printf("As a single character (the first match): %c\n", *sub);
+        printf("As a string: %s\n", sub);
+    } else {
+        printf("Substring not found.\n");
+    }
+
+    return 0;
+ }
 
